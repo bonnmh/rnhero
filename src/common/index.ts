c17e5c4275b26518d3c8fc7fe8781971d901c24d
bon nguyen <bonnmh.hcmue@gmail.com>
2021-10-16 14:38:21 +0700

    config

---
+++ src/common/index.ts
@@ -0,0 +1,11 @@
+
+export * from './handleError';
+export * from './string';
+export * from './scale';
+export * from './array';
+export * from './handle';
+export * from './method';
+export * from './math';
+export * from './type';
+export * from './date';
+export * from './resolver';
---
+++ src/library/utils/i18n/translate.tsx
@@ -0,0 +1,6 @@
+
+import I18n from './i18n';
+
+export function translate(key: string, option?: any) {
+  return key ? I18n.t(key, option) : '';
+}
