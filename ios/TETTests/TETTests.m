7aa47616aeadcaca20e01d2a6fa4f413b558b050
bonnmh <nmhbonc2thptcd@gmail.com>
2021-11-28 22:36:40 +0700

    rename app

--- ios/TETTests/TETTests.m
+++ ios/TETTests/TETTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnheroTests : XCTestCase
+@interface TETTests : XCTestCase
 
 @end
 
-@implementation rnheroTests
+@implementation TETTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
